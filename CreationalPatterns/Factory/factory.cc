ee5fb7b7b31cc204064945f3d351389d8ac9efd7
Jos Collin <jcollin@redhat.com>
2018-01-30 23:08:34 +0530

    Added Factory Pattern
    Signed-off-by: Jos Collin <jcollin@redhat.com>

---
+++ CreationalPatterns/Factory/factory.cc
@@ -0,0 +1,94 @@
+#include<iostream>
+
+enum genre {ROCK, POP, COUNTRY, INVALID};
+
+class MusicRecord {
+ public:
+  virtual void playsong() = 0;
+};
+
+class RockRecord: public MusicRecord {
+ public:
+  void playsong()
+  {
+    std::cout << "Playing: Eagles - Desperado" << std::endl;
+  }
+};
+
+class PopRecord: public MusicRecord {
+ public:
+  void playsong()
+  {
+    std::cout << "Playing: Michael Jackson - Thriller" << std::endl;
+  }
+};
+
+class CountryRecord: public MusicRecord {
+ public:
+  void playsong()
+  {
+    std::cout << "Playing: John Denver - Country Roads" << std::endl;
+  }
+};
+
+class MusicFactory {
+ public:
+  MusicRecord* getmusicrecord(genre gen)
+  {
+    MusicRecord *musicrecord = nullptr;
+    switch(gen)
+    {
+      case ROCK:
+        musicrecord = new RockRecord();
+        break;
+      case POP:
+        musicrecord = new PopRecord();
+        break;
+      case COUNTRY:
+        musicrecord = new CountryRecord();
+        break;
+      default:
+        musicrecord = nullptr;
+        break;
+    }
+    return musicrecord;
+  }
+};
+
+int main()
+{
+  MusicFactory *musicfactory = new MusicFactory();
+
+  int option;
+  std::cout << "Enter 1 for Rock, 2 for Pop, 3 for Country" << std::endl;
+  std::cin >> option;
+
+  genre gen;
+  switch(option)
+  {
+    case 1:
+      gen = ROCK;
+      break;
+    case 2:
+      gen = POP;
+      break;
+    case 3:
+      gen = COUNTRY;
+      break;
+    default:
+      gen = INVALID;
+      break;
+  }
+  
+  MusicRecord *musicrecord = musicfactory->getmusicrecord(gen);
+  if(musicrecord)
+  {
+    musicrecord->playsong();
+  }
+  else
+  {
+    std::cout << "Wrong genre selected" << std::endl;
+  }
+
+  return 0;
+}
