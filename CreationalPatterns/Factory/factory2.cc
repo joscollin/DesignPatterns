c8d9aaf3c7f2dd0ddb0ea9c1bc3ae0c4a3719484
Jos Collin <jcollin@redhat.com>
2018-02-03 09:34:28 +0530

    First Commit - Factory Method2
    Signed-off-by: Jos Collin <jcollin@redhat.com>

---
+++ CreationalPatterns/Factory/factory2.cc
@@ -0,0 +1,98 @@
+#include<iostream>
+#include<vector>
+
+enum genre {ROCK, POP, COUNTRY, INVALID};
+
+class MusicRecord {
+ public:
+  virtual void playsong() = 0;
+  static MusicRecord* getrecord(genre gen);
+};
+
+class RockRecord: public MusicRecord {
+ public:
+  void playsong()
+  {
+    std::cout << "Playing: Eagles - Desperado" << std::endl;
+  }
+};
+
+class PopRecord: public MusicRecord {
+ public:
+  void playsong()
+  {
+    std::cout << "Playing: Michael Jackson - Thriller" << std::endl;
+  }
+};
+
+class CountryRecord: public MusicRecord {
+ public:
+  void playsong()
+  {
+    std::cout << "Playing: John Denver - Country Roads" << std::endl;
+  }
+};
+
+MusicRecord* MusicRecord::getrecord(genre gen)
+{
+    MusicRecord *musicrecord = nullptr;
+    switch(gen)
+    {
+      case ROCK:
+        musicrecord = new RockRecord();
+        break;
+      case POP:
+        musicrecord = new PopRecord();
+        break;
+      case COUNTRY:
+        musicrecord = new CountryRecord();
+        break;
+      default:
+        musicrecord = nullptr;
+        break;
+    }
+    return musicrecord;
+}
+
+int main()
+{
+  std::vector<MusicRecord*> musicrecords;
+
+  int option;
+  while (true) {
+    std::cout << "Enter Rock(1), Pop(2), Country(3), Exit(0)," << std::endl;
+    std::cin >> option;
+
+    genre gen;
+    switch(option)
+      {
+      case 0:
+	exit(0);
+      case 1:
+	gen = ROCK;
+	break;
+      case 2:
+	gen = POP;
+	break;
+      case 3:
+	gen = COUNTRY;
+	break;
+      default:
+	gen = INVALID;
+	break;
+      }
+
+    MusicRecord* musicrecord = MusicRecord::getrecord(gen);
+
+    if (musicrecord)
+      musicrecords.push_back(musicrecord);
+    else
+      std::cout << "wrong genre selected" << std::endl;
+  }
+
+  for (const auto &musicrecord: musicrecords) {
+    musicrecord->playsong();
+  }
+
+  return 0;
+}
